b636060bb4f95de4515d658b6bf1dc2c00765bc5
vijjiwintechsolutions-lab <vijjiwintechsolutions@gmail.com>
2026-08-06 20:54:04 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['sharp', 'pdf-lib'],
-  },
+  serverExternalPackages: ['sharp', 'pdf-lib'],
   images: {
     remotePatterns: [
       {
